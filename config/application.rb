f9d4d9f834ec443b95ef9053f3f7ebf129ea16fd
thethanghn <thethang85@gmail.com>
2013-08-15 10:47:42 +0800

    add initialize on precompile false

--- config/application.rb
+++ config/application.rb
@@ -56,6 +56,7 @@ module Newrail
     # Enable the asset pipeline
     config.assets.enabled = true
     config.assets.paths << "#{Rails.root}/app/assets/font" 
+    config.assets.initialize_on_precompile = false
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
